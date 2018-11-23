28f546703b40727278b601765085161688037047
root <emploi.neveu@gmail.com>
2018-11-23 10:51:51 +0100

    exo6

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+$département = array (
+    69 => 'Nord-Pas-Calais',
+    80 => 'Somme',
+    02 => 'Aisne',
+    59 => 'Nord',
+    60 => 'Oise');
+    echo $département[59];
+    ?>
+  </body>
+</html>
